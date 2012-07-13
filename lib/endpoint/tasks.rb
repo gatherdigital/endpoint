5aa09da1cbf11e757ea77c317b15120c1e7765dc
Adam Williams <adam@thewilliams.ws>
2012-07-13 16:24:48 -0400

    Add needed lib and better message on missing config.

--- lib/endpoint/tasks.rb
+++ lib/endpoint/tasks.rb
@@ -1,12 +1,22 @@
+require 'endpoint/socks'
+
 namespace :endpoint do
   namespace :proxy do
+    def ensure_default_config
+      Endpoint::Socks.default_config.tap do |c|
+        raise "Enpoint::Socks.default_config must be set to use this task." if c.nil? || c.empty?
+      end
+    end
+
     desc 'Start SOCKS proxy over SSH for connecting to firewalled servers.'
     task :start do
+      ensure_default_config
       Endpoint::Socks.start
     end
 
     desc 'Stop SOCKS proxy.'
     task :stop do
+      ensure_default_config
       Endpoint::Socks.stop
     end
   end
